aec99e08e32094f6af57142ebc08cd6fa0f643d8
UltimateSeeSharp <91130013+UltimateSeeSharp@users.noreply.github.com>
2022-02-06 17:06:16 +0100

    Update GetModelsFromBrand.cs

--- Helper/GetModelsFromBrand.cs
+++ Helper/GetModelsFromBrand.cs
@@ -1,29 +1,27 @@
-    static void GetModelsFromBrand()
-    {
-        foreach (var brand in brands)
-        {
-            var links = _autoScoutFetcher.GetHtmlLinks(brand!, pages: 1);
-            var htmls = _autoScoutFetcher.GetHtmlDocuments(links);
-            if (htmls.Count is 0)
-            {
-                File.WriteAllText($@"C:\Users\RIG\source\repos\AutoScout\AutoScout.Dev\Data\BrandModels\NotFound_{brand}.txt", "no models found");
-                continue;
-            }
-
-            var content = htmls[0].DocumentNode.InnerHtml;
-
-            var split1 = content.Split("\"availableModelModelLines\" : ")[1];
-            var split2 = split1.Split("\"topModelIds\"")[0];
-            var newJson = split2.Substring(0, split2.Length - 12);
-
-            var models = JsonConvert.DeserializeObject<List<ModelModel>>(newJson)!
-                .Select(x => x.Name)
-                .Select(x => x!.Replace(' ', '-'))
-                .Select(x => x!.ToLower())
-                .ToList();
-
-            var serialized = JsonConvert.SerializeObject(models, Formatting.Indented);
-
-            File.WriteAllText(@$"C:\Users\RIG\source\repos\AutoScout\AutoScout.Dev\Data\BrandModels\{brand}Models.json", serialized);
-        }
-    }
+static void GetModelsFromBrand()
+{
+   foreach (var brand in brands)
+   {
+       var links = _autoScoutFetcher.GetHtmlLinks(brand!, pages: 1);
+       var htmls = _autoScoutFetcher.GetHtmlDocuments(links);
+       if (htmls.Count is 0)
+       {
+           File.WriteAllText($@"C:\Users\RIG\source\repos\AutoScout\AutoScout.Dev\Data\BrandModels\NotFound_{brand}.txt", "no models found");
+           continue;
+       }
+       
+       var content = htmls[0].DocumentNode.InnerHtml;
+       var split1 = content.Split("\"availableModelModelLines\" : ")[1];
+       var split2 = split1.Split("\"topModelIds\"")[0];
+       var newJson = split2.Substring(0, split2.Length - 12);
+       
+       var models = JsonConvert.DeserializeObject<List<ModelModel>>(newJson)!
+           .Select(x => x.Name)
+           .Select(x => x!.Replace(' ', '-'))
+           .Select(x => x!.ToLower())
+           .ToList();
+       
+       var serialized = JsonConvert.SerializeObject(models, Formatting.Indented);
+       File.WriteAllText(@$"C:\Users\RIG\source\repos\AutoScout\AutoScout.Dev\Data\BrandModels\{brand}Models.json", serialized);
+   }
+}
